b656d8c70fa1c575f9ff5e7cdca152f49453a518
MrChese <gabrielmarcheseee@gmail.com>
2025-04-27 01:07:43 +0000

    coloquei o cavalo

--- xadrez.c
+++ xadrez.c
@@ -3,13 +3,14 @@
 int main() {
     int resultado, movimento;
     int contador = 0, contador2 = 1;
-    char Rainha{10};
+    char Rainha[20];
 
     printf("Seja Bem-Vindo ao Jogo De Xadrez! Para iniciar, escolha uma das peças:\n");
     printf("1. Torre\n");
     printf("2. Bispo\n");
     printf("3. Rainha\n");
-    scanf("%d", &resultado);
+    printf("4. Cavalo\n");
+    scanf("%d", &resultado); 
 
     switch (resultado) {
         case 1:
@@ -91,15 +92,16 @@ int main() {
     printf(" 2. Para Baixo\n");
     printf(" 3. Para Esquerda\n");
     printf(" 4. Para Direita\n");
-    scanf("%d", &movimento);
+    scanf("%s", &Rainha);
 
-    switch (movimento) {
-        case 1:
+    switch (expression)
+    {
+    case 1;
             for (contador2 = 1; contador2 <= 8; contador2++) {
-                printf("A Rainha se moveu para Cima\n");
+                printf("A Rainha se moveu para %s\n", Rainha);
             }
             break;
-        case 2:
+            case 2:
             for (contador2 = 1; contador2 <= 8; contador2++) {
                 printf("A Rainha se moveu para Baixo\n");
             }
@@ -119,8 +121,54 @@ int main() {
             printf("Movimento inválida.\n");
             break;
     }
+            break;
 
+    case 4:
+        printf("A opção escolhida foi Cavalo, Escolha para onde ele deve andar:\n");
+    printf("1. 2 Casa Para Cima e 1 para Direita\n");
+    printf("2. 2 Casa Para Cima e 1 para Esquerda\n");
+    printf("3. 2 Casa Para Baixo e 1 para Direita\n");
+    printf("4. 2 Casa Para Baixo e 1 para Esquerda\n");
+    scanf("%d", &movimento);
+    switch (movimento) {
+        case 1:
+        for (contador = 0; contador2 <= 1; contador++) {
+            for (contador2 = 1; contador2 <= 2; contador2++) {
+                printf("O Cavalo se moveu para Cima\n");
+            }
+            printf("O Cavalo se moveu para Direita\n");
+        }
+         break;
+        case 2:
+        for (contador = 0; contador2 <= 1; contador++) {
+            for (contador2 = 1; contador2 <= 2; contador2++) {
+                printf("O Cavalo se moveu para Cima\n");
+            }
+            printf("O Cavalo se moveu para Esquerda\n");
+        }
+        break;
+        case 3: 
+        for (contador = 0; contador2 <= 1; contador++) {
+            for (contador2 = 1; contador2 <= 2; contador2++) {
+                printf("O Cavalo se moveu para Baixo\n");
+            }
+            printf("O Cavalo se moveu para Direita\n");
+        }
+        break;
+        case 4:
+        for (contador = 0; contador2 <= 1; contador++) {
+            for (contador2 = 1; contador2 <= 2; contador2++) {
+                printf("O Cavalo se moveu para Baixo\n");
+            }
+            printf("O Cavalo se moveu para Esquerda\n");
+        }
+        break;
+
+        default:
+            printf("Movimento inválida.\n");
+            break;
     }
-
+ break;
+}
     return 0;
 }
